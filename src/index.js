1c8e31bd68956d88291196ee43992be212c21deb
davideliason <davethemaker@gmail.com>
2017-11-24 07:49:24 -0800

    refactor json data structure for upload to FB

--- src/index.js
+++ src/index.js
@@ -13,10 +13,7 @@ let logger = createLogger({
 	duration:true
 });
 
-let initialState = {
-	currentUser : ''
-}
-const store = createStore(book_reducer, initialState, applyMiddleware(thunk,logger));
+const store = createStore(book_reducer, {}, applyMiddleware(thunk,logger));
 
 // store.dispatch({type:'SHOW_BOOKS'}); // works correctly
 ReactDOM.render(<Provider store={store}><AppContainer /></Provider>, document.getElementById('root'));

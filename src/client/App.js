e8af9f4799125696ad7da2314e77cca9f5980629
davideliason <davethemaker@gmail.com>
2017-12-01 15:18:26 -0800

    add links to repos and github on App component

--- src/client/App.js
+++ src/client/App.js
@@ -46,6 +46,14 @@ class App extends Component {
                 <h3>Favorite Books</h3>
               </Jumbotron>
           <Row>
+             <Col xs={3}></Col>
+             <Col xs={6}>
+              <a href="https://github.com/davideliason/favorite-books-react-redux-firebase">Repo</a>
+              <a href="https://github.com/davideliason/davideliason.github.io">Github Portfolio</a>
+             </Col>
+             <Col xs={3}></Col>
+
+          <Row>
             <Col xs={3}></Col>
             <Col xs={6}>
 
